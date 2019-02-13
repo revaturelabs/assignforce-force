f6f8f87fb369fc79dd571d5d3b438570c661189f
william.mcneil31996 <william.mcneil31996@gmail.com>
2019-02-13 12:52:41 -0500

    Updated to allow for list of trainers

--- src/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
+++ src/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
@@ -1,5 +1,19 @@
 ({
 	doInit : function(component, event, helper) {
-		
-	}
+        console.log('doInit error');
+		var action = component.get("c.getAllTrainers");
+        console.log('error before call back');
+        action.setCallback(this, function(response){
+           var state = response.getState();
+            console.log('state', state);
+            if(component.isValid() && state === "SUCCESS"){
+                console.log('response' , response.getReturnValue() );
+                component.set('v.trainers', response.getReturnValue() );
+                
+            } else{
+                console.log('Error');
+            }
+        });
+        $A.enqueueAction(action);
+	},
 })
