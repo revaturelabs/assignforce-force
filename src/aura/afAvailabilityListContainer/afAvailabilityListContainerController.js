f09aa9b41322bcc389e1f64c87eb519ea71adbc1
william.mcneil31996 <william.mcneil31996@gmail.com>
2019-02-14 10:30:50 -0500

    Changes between tabs

--- src/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
+++ src/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
@@ -1,8 +1,8 @@
 ({
-	doInit : function(component, event, helper) {
-		var action = component.get("c.getAllTrainers");
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getAllTrainers");
         action.setCallback(this, function(response){
-           var state = response.getState();
+            var state = response.getState();
             if(component.isValid() && state === "SUCCESS"){
                 component.set('v.trainers', response.getReturnValue() );
                 
@@ -11,5 +11,17 @@
             }
         });
         $A.enqueueAction(action);
-	},
+    },
+    trainerClick: function (component, helper) {
+        var isTab1Shown = component.get('v.tab1Shown');
+        if(!isTab1Shown){
+            component.set('v.tab1Shown', true);
+        }
+    },
+    roomClick: function(component, helper){
+        var isTab1Shown = component.get('v.tab1Shown');
+        if(isTab1Shown){
+            component.set('v.tab1Shown', false);
+        }
+    }
 })
--- src/aura/afTrainerAvailabilityListItem/afTrainerAvailabilityListItemController.js
+++ src/aura/afTrainerAvailabilityListItem/afTrainerAvailabilityListItemController.js
@@ -1,7 +1,6 @@
 ({
 	doInit : function(component, event, helper) {
-		var trainer = component.get('v.trainer');
-        console.log('Trainer: ' + trainer);
+		var trainer = component.get('v.trainer');                     
         if(trainer.Available__c==="Available"){
             component.set('v.isAvailable', true);
         } else{
@@ -11,9 +10,7 @@
     selectIsClicked : function(component, event, helper){
         var selectedEvt = component.getEvent('TrainerIsSelected');
         var trainerId = component.get('v.trainer').Id;
-        console.log('trainerId' + trainerId);
         selectedEvt.setParam('trainerId', trainerId);
         selectedEvt.fire();
-        console.log('fired Event');
     }
 })
