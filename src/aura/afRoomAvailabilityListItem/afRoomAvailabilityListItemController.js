1d6cf8d25d29a21190697cb98e926fece51b133f
rajuanbw <rajuanwoodberry@gmail.com>
2019-02-14 13:56:55 -0500

    Available rooms updated

--- src/aura/afRoomAvailabilityListItem/afRoomAvailabilityListItemController.js
+++ src/aura/afRoomAvailabilityListItem/afRoomAvailabilityListItemController.js
@@ -1,5 +1,21 @@
 ({
     doInit : function(component, event, helper) {
-        helper.getRoomList(component);
+        var getRoom = component.get('v.room');
+        
+        if(getRoom.AVAvailability__c == 'Yes'){
+            component.set("v.isYes", "true");
+        }else if(getRoom.AVAvailability__c == 'No'){
+            component.set("v.isNo", "true");
+        }else{
+            component.set("v.isRequest", "true");
+        }
+    },
+    
+    selectedRoom : function(component, event, helper){
+        var roomSelected = component.getEvent('roomSelected');
+        var roomId = component.get('v.room').Id;
+        roomSelected.setParam('roomId', roomId);
+        roomSelected.fire();
+        console.log(roomId);
     }
 })
