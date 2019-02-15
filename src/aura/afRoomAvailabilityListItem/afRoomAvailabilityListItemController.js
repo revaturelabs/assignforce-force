e136091ed3b0194b76ab04fb69e4dd4022645a42
rajuanbw <rajuanwoodberry@gmail.com>
2019-02-15 15:38:00 -0500

    Comments added to code

--- src/aura/afRoomAvailabilityListItem/afRoomAvailabilityListItemController.js
+++ src/aura/afRoomAvailabilityListItem/afRoomAvailabilityListItemController.js
@@ -2,6 +2,8 @@
     doInit : function(component, event, helper) {
         var getRoom = component.get('v.room');
         
+        //Based off of this room's availability, sets the appropriate boolean attribute to true
+        //Done so a certain icon will appear for this room in the application
         if(getRoom.AVAvailability__c == 'Yes'){
             component.set("v.isYes", "true");
         }else if(getRoom.AVAvailability__c == 'No'){
@@ -11,6 +13,8 @@
         }
     },
     
+    //When the select button is clicked, will link the room's Id in this component with the Id parammeter 
+    //need for the for the event
     selectedRoom : function(component, event, helper){
         var roomSelected = component.getEvent('roomSelected');
         var room = component.get('v.room');
