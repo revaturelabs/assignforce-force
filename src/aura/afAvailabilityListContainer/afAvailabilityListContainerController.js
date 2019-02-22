14e9547fd752d92cd31c2c218e54746f6c38bde4
william.mcneil31996 <william.mcneil31996@gmail.com>
2019-02-22 16:26:21 -0500

    handling event but is commented out

--- src/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
+++ src/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
@@ -10,20 +10,16 @@
             }
         });
         $A.enqueueAction(getSkills);
-        
         var getTrainings = component.get("c.getAllTrainings");
         getTrainings.setCallback(this, function(response){
             var state = response.getState();
             if(component.isValid() && state === "SUCCESS"){
                 component.set('v.allTrainings', response.getReturnValue());
-                var t = component.get('v.allTrainings');
-                console.log("Training: " + t);
             } else{
                 console.log('Error');
             }
         });
         $A.enqueueAction(getTrainings);
-        
         var action = component.get("c.getAllTrainers");
         action.setCallback(this, function(response){
             var state = response.getState();
@@ -37,21 +33,27 @@
         $A.enqueueAction(action);
         
         var getRooms = component.get("c.getRooms");
+        console.log('rooms ' + getRooms);
         getRooms.setCallback(this, function(response) {
             var state = response.getState();
             
             if(component.isValid && state === 'SUCCESS'){
+                console.log('state ' + state);
                 component.set("v.rooms", response.getReturnValue());
-                component.set("v.currentLocRooms", response.getReturnValue());
             }else if(state === 'ERROR'){
                 var errors = response.getError();
                 
-                console.log('Error message: ' + errors[0].message);
+                if(errors){
+                    if(errors[0] && errors[0].message){
+                        console.log('Error message: ' + errors[0].message);
+                    }
+                }
             }else{
                 console.log('Unknown error');
             }
         });
         $A.enqueueAction(getRooms);
+        
     },
     trainerClick: function (component, helper) {
         var isTab1Shown = component.get('v.tab1Shown');
@@ -65,7 +67,6 @@
             component.set('v.tab1Shown', false);
         }
     },
-<<<<<<< HEAD
     skillHasChanged: function(component, event, helper){
         //this method handles the event to send the new selected training track to all of the trainers
         var trainers = component.get('v.trainers');
@@ -73,41 +74,13 @@
         component.set('v.selectedTrainingTrack',trainingTrack);
         component.set('v.trainers', null);
         component.set('v.trainers', trainers);
-=======
-    handleLoc : function(component, event, helper) {
-        var loc = event.getParam("location");
-        component.set("v.location", loc);
-        
-        var currentLocRooms = [];
-        var roomsNotAtLoc = [];
-        
-        var rooms = component.get("v.rooms");
-        
-        for(var i = 0; i < rooms.length; i++){
-            if(rooms[i].TrainingLocation__c == loc){
-                currentLocRooms.push(rooms[i]);
-            } else {
-                roomsNotAtLoc.push(rooms[i]);
-            }
-        }
-        if(loc == ""){
-            component.set("v.currentLocRooms", null);
-            component.set("v.currentLocRooms", rooms);
-        } else {
-            component.set("v.currentLocRooms", null);
-            component.set("v.currentLocRooms", currentLocRooms);
-        }
->>>>>>> 8affc68f2d3956c0c9333a080c5e32b805e38748
     },
-    
     dateHasChanged: function(component, event, helper){
         //This method checks the start date and end date of every training within the dates selected from the afBatchFormDateEvent and changes the availability of the trainer accordingly
         var trainers = component.get('v.trainers');
         var trainings = component.get('v.allTrainings');
         var startDate = new Date(event.getParam('startDate'));
         var endDate = new Date(event.getParam('endDate'));
-        var currentLocRooms = component.get('v.currentLocRooms');
-        
         for(var i=0; i<trainers.length; i++){
             for (var j = 0; j < trainings.length; j++) {
                 if(trainers[i].Id == trainings[j].Trainer__c || trainers[i].Id == trainings[j].CoTrainer__c) {
@@ -125,35 +98,20 @@
                 }
             }
         }
-         for(var i=0; i<currentLocRooms.length; i++){
-            for (var j = 0; j < trainings.length; j++) {
-                if(currentLocRooms[i].Id == trainings[j].TrainingRoom__c) {
-                    var prevStart = new Date(trainings[j].StartDate__c);
-                    var prevEnd = new Date(trainings[j].EndDate__c);
-                    
-                    if((prevStart <= startDate && startDate <= prevEnd) || 
-                       (prevStart <= endDate  && endDate <= prevEnd) || 
-                       (prevStart >= startDate    && endDate >= prevEnd)){
-                        currentLocRooms[i].AVAvailability__c = "No";
-                        break;
-                    }else {
-                        currentLocRooms[i].AVAvailability__c = "Yes";
-                    }
-                }
-            }
-        }
-        component.set('v.startDate', startDate);
-        component.set('v.endDate', endDate);
-        
-        component.set('v.currentLocRooms', null);
-        component.set('v.currentLocRooms', currentLocRooms);
         component.set('v.trainers', null);
         component.set('v.trainers', helper.sortTrainers(helper.sortTrainers(trainers)));
-        
     },
-    
-    handleDate : function(component, event, helper){
-        var start = event.getParam("startDate");
-        var end = event.getParam("endDate");
-    }
+    /*trainingTrackCreated: function(component, event, helper){
+        var trainings = component.get('v.allTrainings');
+        var trainers = component.get('v.trainers');
+        var currentLocRooms = component.get('v.currentLocRooms');
+        var newTraining = event.getParam('newBatch');
+        trainings.push(newTraining);
+        component.set('allTrainings', trainings);
+        component.set('v.trainers', null);
+        component.set('v.trainers', helper.sortTrainers(helper.sortTrainers(trainers)));
+        component.set('v.currentLocRooms', null);
+        component.set('v.currentLocRooms', currentLocRooms);
+        
+    }*/
 })
