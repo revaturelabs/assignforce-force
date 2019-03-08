3b6f2197e6040a79c896203cfb8058506d512417
evajoy <evajlahtinen@gmail.com>
2019-03-08 12:53:03 -0500

    Will's Process, code comments, and removing unecessary files

--- src/aura/afBatchTimeline/afBatchTimelineHelper.js
+++ src/aura/afBatchTimeline/afBatchTimelineHelper.js
@@ -1,6 +1,7 @@
 ({
     getNames : function(component, event)
     {
+        //Calls the getTrainers function on the apex controller, returning all of the trainer names
         var action = component.get("c.getTrainers");
         action.setCallback(this, function(response){
             var state = response.getState();
@@ -8,6 +9,7 @@
             {
                 var trainerNames = response.getReturnValue();              
                 this.fireEvent(component, event, trainerNames);
+                //After we get the names from the apex, we fire this event
             }
             else if(state === 'ERROR')
             {
@@ -19,6 +21,7 @@
     
     fireEvent : function(component, event, trainerNames)
     {
+        //Using the trainer names, we then fire the createJSON event to wrap the data
         var createJSON = component.getEvent("CreateJSON");
         var newJSONstring = trainerNames.toString();
         var newArray = newJSONstring.split(",");
@@ -29,30 +32,38 @@
         createJSON.fire();
     },
     
+    //Deprecated, the function of this was supposed to be for when new data gets sent to the chart,
+    //We would just update the chart with the new data, then recreate the chart
     InitUpdate : function(component, event, names)
     {
         component.set('v.data', event.getParam('data'));
         var names = component.get('v.trainers');
         this.createChart(component, event, names);
     },
+    //This is the last function to be called when creating the chart
     createChart : function(component, event, names) {
         var jsonData = component.get("v.data");
+        //This variable will hold all of the data for all of the series
         var seriesObj = [];
+        //parses the data we've set, so that we can work with it
         var dataObj = JSON.parse(jsonData);
+        //Sometimes we're passed in the names, others this is being called from an event.
         if(names == null){
             var trainers = event.getParam("yAxisNames");
         }
         else{
             var trainers = names;
         }
-        var trainerAssignment = [];
         
+        // seriesNames will hold all of the batch names
         var seriesNames = [];
-        var seriesData = [];
-        var freeTimeData = [];
+        // Not used right now, but this could be used to only show on the y-axis the names 
+        //of the trainers that appear in the data we're creating the chart with
         var trainersInData = [];
+        
         for(var i = 0; i < dataObj.length; i++)
         {
+            //This will create the start and end dates (x, and x2 respectively for each batch)
             var year = dataObj[i].x.substring(0,4);
             var month = dataObj[i].x.substring(5,7) - 1;
             var day = dataObj[i].x.substring(8);
@@ -61,12 +72,16 @@
             var month2 = dataObj[i].x2.substring(5,7) - 1;
             var day2 = dataObj[i].x2.substring(8);
             dataObj[i].x2 = Date.UTC(year2,month2,day2);
+            //Get the name of the batch type, then remove it from the object
             var seriesName = dataObj[i].series;
             delete dataObj[i].series;
+            //This is where we could do the name checking/grabbing 
             if(!trainersInData.includes(dataObj[i].trainerName)){
                  trainersInData.push(dataObj[i].trainerName);
-			}
+            }
+            //Then remove it from the data
             delete dataObj[i].trainerName;
+            //If we already have a batch of this name, add it to the data for that series (batch)
             if(seriesNames.includes(seriesName))
             {
                 for(var c = 0; c < seriesObj.length; c++)
@@ -77,10 +92,11 @@
                     }
                 }
             }
+            //Otherwise it's a new batch type, we'll create a new series for it
             else
             {
                 seriesNames.push(seriesName);
-                seriesData.push(dataObj[i]);
+                //You can check the highcharts api document for how to use these attributes, and what values they can be
                 seriesObj.push({'name' : seriesName, 'pointWidth' : 30,  'data' : [{'x' : dataObj[i].x, 'x2' : dataObj[i].x2, 'y' : dataObj[i].y, 'color' : dataObj[i].color}], 'dataLabels': {
                     enabled: true,
                     style:
@@ -101,30 +117,40 @@
             
             
         }
+        // Holds all of the data for the free times
         var freeTimeData = [];
+        // Holds all of the trainers we've calculated free time for
         var trainersDone = [];
+        // Loops through all of the batches we're creating the chart with
         for(var i = 0; i < dataObj.length; i++)
         {
+            //If the trainer wasn't done yet, then we'll calculate free time for that trainer
             if(!trainersDone.includes(dataObj[i].y)){
                 var currentTrainer = dataObj[i].y;
                 trainersDone.push(currentTrainer);
+                //This will hold all of the batches for the trainer
                 var currentTrainerBatches = [];
-                //var currentTrainerFreeTime = [];
+                
+                //Loops through all the data again (This probably is not the most optimal way to do it)
                 for(var j = i; j < dataObj.length; j++)
                 {
+                    //If we find a batch taught by that trainer, and we haven't found this batch before, add it to our array
                     if(currentTrainer == dataObj[j].y && !currentTrainerBatches.includes(dataObj[j])){
                         currentTrainerBatches.push(dataObj[j]);
                     }
                     
                 }
+                //Loop through all of the batches we grabbed
                 for(var k = 0 ; k < currentTrainerBatches.length ; k++){
+                    //Must be more than two batches for there to be free time
                     if(currentTrainerBatches.length < 2){
                         break;
                     } else {
-                        
+                        //This creates the free time itself, if the end date on the first batch is less than the start date on the second batch
                         if(currentTrainerBatches[k].x2 < currentTrainerBatches[k+1].x){
                             freeTimeData.push({'x' : currentTrainerBatches[k].x2, 'x2' : currentTrainerBatches[k+1].x, 'y' : currentTrainer, 'color' : '#FFFFFF'});
                         }
+                        //If it's the last batch, there can be nothing after it (We grab it in order by start date)
                         if(k == currentTrainerBatches.length - 2){
                             break;
                         }
@@ -132,7 +158,7 @@
                 }
             }      
         }
-        
+        //Creates a series called free time with the data we just created
         seriesObj.push({'name' : 'Free Time', 'pointWidth' : 30, 'data' : freeTimeData, 'fill' : '#FFFFFF', 'dataLabels' : {
             enabled : true,
             style:
@@ -193,6 +219,7 @@
                 },
                
             },
+            //This is setting the data for the chart to all of the data we've calculated before (with each seriesObj.push)
             series: //[ dataObj ]
             seriesObj            
         });
