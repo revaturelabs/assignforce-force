fe8ca98f8cc787bd899f213d2215a8e7b4662d91
matthewruiz <matthewruiz620@gmail.com>
2019-04-18 13:04:26 -0400

    test

--- force-app/main/default/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
+++ force-app/main/default/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
@@ -1,4 +1,5 @@
 ({
+    
     doInit: function(component, event, helper){
         //INITIALIZE TRAINERS WITH INITIAL SORT
         var filterController = component.get("c.sortTrainersBySelectedCategories");
