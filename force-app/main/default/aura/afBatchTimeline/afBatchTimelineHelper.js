3903f07885bcd3c29654e6fddb42ca7d56b2db8e
David White <whited9339@gmail.com>
2019-04-19 14:15:25 -0400

    Chart now expands vertically

--- force-app/main/default/aura/afBatchTimeline/afBatchTimelineHelper.js
+++ force-app/main/default/aura/afBatchTimeline/afBatchTimelineHelper.js
@@ -165,7 +165,8 @@
         var charts = new Highcharts.chart({
             chart: {
                 renderTo: component.find("container").getElement(),
-                type: 'xrange'
+                type: 'xrange',
+                height: '100%'
             },
             title: {
                 text : component.get('v.chartTitle'),
