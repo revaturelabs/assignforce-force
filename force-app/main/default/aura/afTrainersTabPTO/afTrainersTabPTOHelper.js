587a8e479ad99436f561c6060eec4895ba234ce9
Christopher Saul Jr <cwsauljr@gmail.com>
2019-04-19 09:57:21 -0400

    Don't display empty lightning tables for trainer PTO

--- force-app/main/default/aura/afTrainersTabPTO/afTrainersTabPTOHelper.js
+++ force-app/main/default/aura/afTrainersTabPTO/afTrainersTabPTOHelper.js
@@ -90,6 +90,18 @@
             futurePTOs.push(this.addToArray(tempObj , endDateString, startDateString));
         }
         
+        // Based on the emptiness of lists for each of current and upcoming PTO we set booleans to display or not
+        if (PTOs.length > 0){
+            component.set('v.hasCurrentPTO', true);
+        } else{
+            component.set('v.hasCurrentPTO', false);
+        }
+        if (futurePTOs.length > 0){
+            component.set('v.hasUpcomingPTO', true);
+        } else{
+            component.set('v.hasUpcomingPTO', false);
+        }
+
         //sets the values from trainings to current PTOs datatable and futureTrainings to upcoming PTOs table
         component.set('v.empCurrentPTODataset', PTOs);
         component.set('v.empFuturePTODataset', futurePTOs);
