0a84d74a63618534644dca4a3ac777a486e8b781
Tim van der Graaff <timvdg45@gmail.com>
2019-04-23 13:17:07 -0400

    The JSON array is reconstructed with for loops.

--- force-app/main/default/aura/FilterChartComponent/FilterChartComponentHelper.js
+++ force-app/main/default/aura/FilterChartComponent/FilterChartComponentHelper.js
@@ -50,10 +50,10 @@
             {
                 alert("There was an error with your Callback.");
             }
-                else
-                {
-                    alert("There is an unknown error. Please contact your Administrator.");
-                }
+            else
+            {
+                alert("There is an unknown error. Please contact your Administrator.");
+            }
         });
         
         $A.enqueueAction(action);
@@ -83,10 +83,12 @@
     filterByChange : function(component, event)
     {
         var changeValue = component.get('v.value');//event.getParam("value");
+        
         if(changeValue == undefined)
         {
             changeValue = [];
         }
+        
         var action; 
         
         component.set('v.selectedLocations', changeValue);
@@ -94,6 +96,7 @@
         if(component.get('v.selectedQuarter') != 1 && changeValue.length != 0)
         {
             action = component.get('c.filterTrainingsByYearLocationQuarter');
+            
             action.setParams({
                 'location' : Locations,
                 'year' : component.get('v.selectedYear'),
@@ -103,53 +106,226 @@
         else if(changeValue.length != 0 && component.get('v.selectedQuarter') == 1)
         {
             action = component.get('c.filterTrainingsByYearLocation');
+            
             action.setParams({
                 'location' : Locations,
                 'year' : component.get('v.selectedYear')
             });
         }
-            else if(component.get('v.selectedQuarter') != 1)
-            {
-                action = component.get('c.filterTrainingsByYearQuarter');
-                action.setParams({
-                    'year' : component.get('v.selectedYear'),
-                    'quarter' : component.get('v.selectedQuarter')
-                });
-            }
-                else
-                {
-                    action = component.get('c.filterTrainingsByYear');
-                    action.setParams({
-                        'year' : component.get('v.selectedYear')
-                    });
-                }
-       // console.log("action: " + JSON.stringify(action));
+        else if(component.get('v.selectedQuarter') != 1)
+        {
+            action = component.get('c.filterTrainingsByYearQuarter');
+            
+            action.setParams({
+               	'year' : component.get('v.selectedYear'),
+                'quarter' : component.get('v.selectedQuarter')
+            });
+        }
+        else
+        {
+         	action = component.get('c.filterTrainingsByYear');
+            
+            action.setParams({
+                'year' : component.get('v.selectedYear')
+            });
+        }
+       	
+        // console.log("action: " + JSON.stringify(action));
         action.setCallback(this, function(response){
             var state = response.getState();
             if(/*component.isValid() &&*/ state === 'SUCCESS')
             {
-                var data = response.getReturnValue();
+                var data = JSON.parse(response.getReturnValue());
+                
                 this.fireChangeToChart(component, event, data);
             }
             else if(state === 'ERROR') {
                 var errors = response.getError();
-                        if (errors) {
-                            if (errors[0] && errors[0].message) {
-                                console.log('Error message: ' + errors[0].message);
-                            }
-                        }
+                
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log('Error message: ' + errors[0].message);
+                    }
+                }
             } else {
                 console.log("state " + state);
                 alert('There seems to have been an error with the callback for the filter. Please Contact your administrator, bro.');
             }
         });
+        
         $A.enqueueAction(action);
     },
     fireChangeToChart : function(component, event, data)
     {
-        component.set('v.dataTemp', data);
+        var newObject = "";
+        var findTrainingTrack = [[], [], [], [], [], [], []];
+        var writeObject = "";
+        
+        for (var i = 0; i < data.length; i++) {
+			
+			findTrainingTrack[0][i] = data[i]["y"];
+			findTrainingTrack[1][i] = data[i]["x2"];
+			findTrainingTrack[2][i] = data[i]["x"];
+			findTrainingTrack[3][i] = data[i]["trainerName"];
+			findTrainingTrack[4][i] = data[i]["series"];
+			findTrainingTrack[5][i] = data[i]["location"];
+			findTrainingTrack[6][i] = data[i]["color"];
+			
+			for (var j = 0; j < findTrainingTrack.length; j++) {
+				
+				if (findTrainingTrack[4][i] == "PTO") {
+					
+					if (findTrainingTrack[0][i] == findTrainingTrack[0][j] && findTrainingTrack[4][i] != findTrainingTrack[4][j]) {
+						
+						writeObject += "{";
+						writeObject += "\"y\":";
+						writeObject += findTrainingTrack[0][i];
+						writeObject += ",\"x2\":";
+				
+						if (findTrainingTrack[1][i] == null) {
+					
+							writeObject += findTrainingTrack[1][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[1][i] + "\"";
+						}
+				
+						writeObject += ",\"x\":";
+				
+						if (findTrainingTrack[2][i] == null) {
+					
+							writeObject += findTrainingTrack[2][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[2][i] + "\"";
+						}
+				
+						writeObject += ",\"trainerName\":";
+				
+						if (findTrainingTrack[3][i] == null) {
+					
+							writeObject += findTrainingTrack[3][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[3][i] + "\"";
+						}
+				
+						writeObject += ",\"series\":";
+				
+						if (findTrainingTrack[4][i] == null) {
+					
+							writeObject += findTrainingTrack[4][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[4][i] + "\"";
+						}
+				
+						writeObject += ",\"location\":";
+				
+						if (findTrainingTrack[5][i] == null) {
+					
+							writeObject += findTrainingTrack[5][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[5][i] + "\"";
+						}
+				
+						writeObject += ",\"color\":";
+				
+						if (findTrainingTrack[6][i] == null) {
+					
+							writeObject += findTrainingTrack[6][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[6][i] + "\"";
+						}
+				
+						writeObject += "},";
+					}
+				} else {
+					
+					if (findTrainingTrack[0][i] == findTrainingTrack[0][j]) {
+						
+						writeObject += "{";
+						writeObject += "\"y\":";
+						writeObject += findTrainingTrack[0][i];
+						writeObject += ",\"x2\":";
+				
+						if (findTrainingTrack[1][i] == null) {
+					
+							writeObject += findTrainingTrack[1][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[1][i] + "\"";
+						}
+				
+						writeObject += ",\"x\":";
+				
+						if (findTrainingTrack[2][i] == null) {
+					
+							writeObject += findTrainingTrack[2][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[2][i] + "\"";
+						}
+				
+						writeObject += ",\"trainerName\":";
+				
+						if (findTrainingTrack[3][i] == null) {
+					
+							writeObject += findTrainingTrack[3][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[3][i] + "\"";
+						}
+				
+						writeObject += ",\"series\":";
+				
+						if (findTrainingTrack[4][i] == null) {
+					
+							writeObject += findTrainingTrack[4][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[4][i] + "\"";
+						}
+				
+						writeObject += ",\"location\":";
+				
+						if (findTrainingTrack[5][i] == null) {
+					
+							writeObject += findTrainingTrack[5][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[5][i] + "\"";
+						}
+				
+						writeObject += ",\"color\":";
+				
+						if (findTrainingTrack[6][i] == null) {
+					
+							writeObject += findTrainingTrack[6][i];
+						} else {
+					
+							writeObject += "\"" + findTrainingTrack[6][i] + "\"";
+						}
+				
+						writeObject += "},";
+					}
+				}
+			}
+		}
+		
+		writeObject += "{}";
+
+		newObject += "[" + writeObject.replace(/,{}/g, "") + "]";
+        
+        console.log(newObject);
+        
+        component.set('v.dataTemp', newObject);
+        
         var UpdateChart = $A.get('e.c:UpdateChartEvent');
+        
         UpdateChart.setParams({'data' : component.get('v.dataTemp')});
         UpdateChart.fire();
-    },
+    }
 })
