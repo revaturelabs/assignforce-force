74eba56a7c2730bde83b185999e01c9b1adce058
Matt Wright <mattwright0325@gmail.com>
2019-04-26 10:24:15 -0400

    Removed ALL references to WrapTrainingToJSON

--- force-app/main/default/aura/afBatchTimeline/afBatchTimelineController.js
+++ force-app/main/default/aura/afBatchTimeline/afBatchTimelineController.js
@@ -17,7 +17,7 @@
     
     createJSON : function(component, event, helper)
     {
-        var action = component.get("c.wrapTrainingToJSON");
+        var action = component.get("c.createTrainingJSON");
         action.setCallback(this, function(response){
             var state = response.getState();
             if(component.isValid() && state === 'SUCCESS'){
